4dd4c6fef1d41c7d5d0959f86a4eddd124f767c8
Zhang Minghan <zmh@lightxi.com>
2025-09-14 04:01:32 +0800

    feat: update tips feature

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-muted/50 text-secondary-foreground shadow-sm hover:bg-muted",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
